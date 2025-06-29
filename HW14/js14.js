0e3861343afd9001e02f2f3eb1f0b6da25eb9d98
VadimBekin <vadimbeckin@yandex.by>
2025-06-29 16:20:52 +0300

    HW14 поправил

--- HW14/js14.js
+++ HW14/js14.js
@@ -21,26 +21,26 @@
 // console.log(result.sum3);
 
 //z2
-function find(arr) {
-    if (!arr ||arr.length === 0) {
-        return undefined;
-    }
-    let sum = arr.reduce((acc, num) => acc + num, 0);
-    let average = sum / arr.length;
-    let nElement = arr[0];
-    let min = Math.abs(arr[0] - average);
-    for (let i = 1; i <= arr.length; i++) {
-        let dif = Math.abs(arr[i] - average);
-        if (dif < min) {
-            min = dif;
-            nElement = arr[i];
-        }
-    }
-    return nElement;
-}
-let num = [1, 8, 3, 10, 4, 7];
-let fin = find(num);
-console.log(fin);
+// function find(arr) {
+//     if (!arr ||arr.length === 0) {
+//         return undefined;
+//     }
+//     let sum = arr.reduce((acc, num) => acc + num, 0);
+//     let average = sum / arr.length;
+//     let nElement = arr[0];
+//     let min = Math.abs(arr[0] - average);
+//     for (let i = 1; i <= arr.length; i++) {
+//         let dif = Math.abs(arr[i] - average);
+//         if (dif < min) {
+//             min = dif;
+//             nElement = arr[i];
+//         }
+//     }
+//     return nElement;
+// }
+// let num = [1, 8, 3, 10, 4, 7];
+// let fin = find(num);
+// console.log(fin);
 
 
 //z3
@@ -61,22 +61,22 @@ console.log(fin);
 
 //z4
 
-// function right (arr, n, fillValue) {
-//     if (n <=0 || !Array.isArray(arr)) {
-//         return arr.slice;
-//     }
-//     const len = arr.length;
-//     if (len === 0) return arr.slice();
-//     n = Math.min(n, len);
-//     const removedElements = arr.splice(len - n, n);
-//     for (let i = 0; i < n; i++) {
-//         arr.unshift(fillValue);
-//     }
-//     return arr;
-// }
-// let myArray = [1, 2, 3, 4, 5];
-// let  rightArray = right(myArray, 2, 0);
-// console.log(rightArray);
+function right (arr, n, fillValue) {
+    if (n <=0 || !Array.isArray(arr)) {
+        return arr;
+    }
+    const len = arr.length;
+    if (len === 0) return arr();
+    n = Math.min(n, len);
+    const removedElements = arr.splice(len - n, n);
+    for (let i = 0; i < n; i++) {
+        arr.unshift(fillValue);
+    }
+    return arr;
+}
+let myArray = [1, 2, 3, 4, 5];
+let  rightArray = right(myArray, 5, 3);
+console.log(rightArray);
 
 
 //z5
